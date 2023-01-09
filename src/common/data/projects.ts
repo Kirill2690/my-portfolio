217810e44c4ef12edbf9ea69dc8795150f2dcc80
Kirill2690 <kirillmuravev2690@gmail.com>
2023-01-09 18:28:02 +0300

    add:new project

--- src/common/data/projects.ts
+++ src/common/data/projects.ts
@@ -2,19 +2,32 @@ import counter from '../../images/counter.png';
 import social_network from '../../images/social-network.webp';
 import storybook_image from '../../images/storybook_image.jpg'
 import pizza from '../../images/pizza_logo.png'
+import todolist from '../../images/todolist.jpeg'
+import cards from '../../images/learn.jpg'
 
 
 export const projectsData = [
-   /* {
-        id: '1',
-        title: 'Todo List',
-        description: 'Technology stack: ReactJS, TypeScript, Redux Toolkit, RestAPI, Axios, Redux-Thunk, Material UI, Unit testing, Storybook',
-        backgroundImage: `url(${todolist})`,
-        repoLink: 'https://github.com/Zouker/todolist-trello',
-        demoLink: 'https://zouker.github.io/todolist-trello/'
-    },*/
     {
         id: '1',
+        title: 'Todo List',
+        description: 'Technology stack: ReactJS, TypeScript, RestAPI, Axios, Redux-Thunk, Material UI, Unit testing',
+        backgroundImage: `url(${todolist})`,
+        repoLink: 'https://github.com/Kirill2690/Todolist-app',
+        demoLink: 'https://kirill2690.github.io/Todolist-app/'
+    },
+
+    {
+        id: '2',
+        title: 'Project Cards',
+        description: 'Technology stack: ReactJS, TypeScript, RestAPI, Axios, Redux-Thunk, Material UI, Formik',
+        backgroundImage: `url(${cards})`,
+        repoLink: 'https://github.com/Kirill2690/cards-front',
+        demoLink: 'https://Kirill2690.github.io/cards-front'
+
+    },
+
+    {
+        id: '3',
         title: 'Social Network',
         description: 'Technology stack: ReactJS, TypeScript, Redux, RestAPI, Axios, Redux-Thunk, React-Router-Dom, Redux-Form, Formik, Ant Design',
         backgroundImage: `url(${social_network})`,
@@ -22,7 +35,7 @@ export const projectsData = [
         demoLink: 'https://Kirill2690.github.io/social_network'
     },
     {
-        id: '2',
+        id: '4',
         title: 'Counter with settings block',
         description: 'Technology stack: ReactJS, TypeScript, Redux, Unit testing, CSS',
         backgroundImage: `url(${counter})`,
@@ -30,21 +43,23 @@ export const projectsData = [
         demoLink: 'https://kirill2690.github.io/counter_and_redux/'
     },
     {
-        id: '3',
+        id: '5',
         title: 'My storybook components',
         description: 'Technology stack: ReactJS, TypeScript, Storybook, CSS',
         backgroundImage: `url(${storybook_image})`,
         repoLink: 'https://github.com/Kirill2690/my-storybook.git',
-        demoLink: ''
+        demoLink: 'https://my-storybook-ojwtd3bud-kirill2690.vercel.app/'
     },
     {
-        id: '4',
+        id: '6',
         title: 'Solo Pizza is an application for delivering delicious pizza in your city.',
         description: 'Technology stack: ReactJS, TypeScript, SCSS',
         backgroundImage: `url(${pizza})`,
         repoLink: 'https://github.com/Kirill2690/pizza-app.git',
         demoLink: 'https://Kirill2690.github.io/pizza-app'
     },
+
+
 
 
 
